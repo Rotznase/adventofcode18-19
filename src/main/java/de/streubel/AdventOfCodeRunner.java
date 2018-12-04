53162746cd2c00ee5d16f1216d854e950871a922
Gerd Streubel <gst@pixel.de>
2018-12-04 21:47:27 +0100

    Auslagerung der Inputstrings in Dateien.

--- src/main/java/de/streubel/AdventOfCodeRunner.java
+++ src/main/java/de/streubel/AdventOfCodeRunner.java
@@ -1,5 +1,9 @@
 package de.streubel;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public abstract class AdventOfCodeRunner {
 
     public static void main(String[] args) throws Exception {
@@ -8,8 +12,11 @@ public abstract class AdventOfCodeRunner {
         final String className = String.format("de.streubel.aoc18.Day%02d", dayNr);
         final Class<?> clazz = Class.forName(className);
         AdventOfCodeRunner runner = (AdventOfCodeRunner) clazz.newInstance();
-        runner.run();
+
+        List<String> input = Files.readAllLines(Paths.get("./src/main/java/de/streubel/aoc18/resources/input"+clazz.getSimpleName()+".txt"));
+
+        runner.run(input);
     }
 
-    public abstract void run();
+    public abstract void run(List<String> input) throws Exception;
 }
