01b210be1f487ba64f48764c12c6cd4d1785f341
Gerd Streubel <gst@pixel.de>
2018-12-05 08:00:04 +0100

    Day05, Part 2

--- src/main/java/de/streubel/aoc18/Day05.java
+++ src/main/java/de/streubel/aoc18/Day05.java
@@ -3,8 +3,6 @@ package de.streubel.aoc18;
 import de.streubel.AdventOfCodeRunner;
 
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 public class Day05 extends AdventOfCodeRunner {
@@ -12,16 +10,31 @@ public class Day05 extends AdventOfCodeRunner {
     @Override
     public void run(List<String> input) {
 
-        Set<Character> availChars = new HashSet<>();
-        for (char c: input.get(0).toCharArray()) {
-            availChars.add(c);
-        }
+        String s;
 
-        String s = input.get(0);
+        s = react(input.get(0));
+        System.out.println("Result Part 1: "+s.length());
+
+
+        int minLength = Integer.MAX_VALUE;
+        for (char c : "abcdefghijklmnopqrstuvwxyz".toCharArray()) {
+            char C = Character.toUpperCase(c);
+            String regex = "[" + c + C + "]";
+            s = input.get(0);
+            s = s.replaceAll(regex, "");
+            s = react(s);
+            if (s.length() < minLength) {
+                minLength = s.length();
+            }
+        }
+        System.out.println("Result Part 2: "+minLength);
+    }
+
+    private String react(String s) {
         boolean b = true;
         while (b) {
             b = false;
-            for (Character c : availChars) {
+            for (Character c : getAvailableChars(s)) {
                 char oppositePolarity = Character.isLowerCase(c) ? Character.toUpperCase(c) : Character.toLowerCase(c);
                 String regex = "" + c + oppositePolarity;
 
@@ -31,8 +44,15 @@ public class Day05 extends AdventOfCodeRunner {
                 b = b || lengthAfter != lengthBefore;
             }
         }
+        return s;
+    }
 
-        System.out.println("Result Part 1: "+s.length());
+    private Set<Character> getAvailableChars(String s) {
+        Set<Character> availChars = new HashSet<>();
+        for (char c: s.toCharArray()) {
+            availChars.add(c);
+        }
+        return availChars;
     }
 
 }
