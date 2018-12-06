98706e57bccb734062300ab5c1c0013140274ae9
Gerd Streubel <gst@pixel.de>
2018-12-06 18:34:44 +0100

    Refactoring Part 1.

--- src/main/java/de/streubel/aoc18/Day06.java
+++ src/main/java/de/streubel/aoc18/Day06.java
@@ -13,42 +13,17 @@ public class Day06 extends AdventOfCodeRunner {
 
     @Override
     public void run(List<String> input) {
-        Set<Coord> places = new HashSet<>();
-        for (String s: input) {
-            String[] split = s.split(", *");
-            Coord c = new Coord(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
-            places.add(c);
-        }
 
-        int minX = Integer.MAX_VALUE;
-        int maxX = Integer.MIN_VALUE;
-        int minY = Integer.MAX_VALUE;
-        int maxY = Integer.MIN_VALUE;
-        for (Coord c: places) {
-            if (c.x < minX) minX = c.x;
-            if (c.x > maxX) maxX = c.x;
-            if (c.y < minY) minY = c.y;
-            if (c.y > maxY) maxY = c.y;
-        }
+        Set<Coord> places = parseInput(input);
 
-        SetMultimap<Coord, Coord> coordToAreaMapping = HashMultimap.create();
-        for (int x=minX; x<=maxX; x++) {
-            for (int y=minY; y<=maxY; y++) {
-                Coord c = new Coord(x, y);
-                Coord c2 = c.getNearby(places);
-                if (c2 != null) {
-                    coordToAreaMapping.put(c2, c);
-                }
-            }
-        }
+        Boundary boundary = getBoundary(places);
 
+        SetMultimap<Coord, Coord> coordToAreaMapping = getAreaMapping(places, boundary);
 
         int maxArea = 0;
-        Coord candiate = null;
         for (Coord c: coordToAreaMapping.keySet()) {
             if (coordToAreaMapping.get(c).size() > maxArea) {
                 maxArea = coordToAreaMapping.get(c).size();
-                candiate = c;
             }
         }
 
@@ -56,6 +31,47 @@ public class Day06 extends AdventOfCodeRunner {
 
     }
 
+    private SetMultimap<Coord, Coord> getAreaMapping(Set<Coord> places, Boundary boundary) {
+        SetMultimap<Coord, Coord> coordToAreaMapping = HashMultimap.create();
+        for (int x=boundary.minX; x<=boundary.maxX; x++) {
+            for (int y=boundary.minY; y<=boundary.maxY; y++) {
+                Coord c = new Coord(x, y);
+                Coord c2 = c.getNearby(places);
+                if (c2 != null) {
+                    coordToAreaMapping.put(c2, c);
+                }
+            }
+        }
+        return coordToAreaMapping;
+    }
+
+    private Boundary getBoundary(Set<Coord> places) {
+        Boundary r = new Boundary();
+        for (Coord c: places) {
+            if (c.x < r.minX) r.minX = c.x;
+            if (c.x > r.maxX) r.maxX = c.x;
+            if (c.y < r.minY) r.minY = c.y;
+            if (c.y > r.maxY) r.maxY = c.y;
+        }
+        return r;
+    }
+
+    private Set<Coord> parseInput(List<String> input) {
+        Set<Coord> places = new HashSet<>();
+        for (String s: input) {
+            String[] split = s.split(", *");
+            Coord c = new Coord(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
+            places.add(c);
+        }
+        return places;
+    }
+
+    private static class Boundary {
+        int minX = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxY = Integer.MIN_VALUE;
+    }
 
     private static class Coord {
         int x;
