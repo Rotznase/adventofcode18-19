cc183023ef61f646b7bdb5306943164aaf2f74c4
Gerd Streubel <gst@pixel.de>
2018-12-08 09:53:10 +0100

    Day08, Part 2

--- src/main/java/de/streubel/aoc18/Day08.java
+++ src/main/java/de/streubel/aoc18/Day08.java
@@ -2,7 +2,6 @@ package de.streubel.aoc18;
 
 import de.streubel.AdventOfCodeRunner;
 
-import java.text.NumberFormat;
 import java.util.*;
 
 
@@ -14,13 +13,14 @@ public class Day08 extends AdventOfCodeRunner {
         Node rootNode = parseInput(new Scanner(input.get(0)));
 
         System.out.println("Result Part 1: "+rootNode.calculateMetadata());
+        System.out.println("Result Part 2: "+rootNode.value());
 
     }
 
     private static Node parseInput(Scanner scanner) {
         int nrOfChildren = scanner.nextInt();
         int nrOfMetadata = scanner.nextInt();
-        Node n = new Node("A", nrOfChildren, nrOfMetadata);
+        Node n = new Node(nextName(), nrOfChildren, nrOfMetadata);
 
         for (int i=0; i<nrOfChildren; i++) {
             Node child = parseInput(scanner);
@@ -59,5 +59,30 @@ public class Day08 extends AdventOfCodeRunner {
                 sum += i;
             return sum;
         }
+
+        int value() {
+            int value = 0;
+            if (children.length == 0) {
+                value = sumMetadata();
+            } else {
+                for (int md : metadata) {
+                    int index = md - 1;
+                    if (index >= 0 && index < children.length) {
+                        value += children[index].value();
+                    }
+
+                }
+            }
+            return value;
+        }
+
+        public String toString() {
+            return name;
+        }
+    }
+
+    static char currentName = 'A';
+    private static String nextName() {
+        return ""+currentName++;
     }
 }
