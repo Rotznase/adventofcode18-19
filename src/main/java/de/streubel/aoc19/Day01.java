e8f29cfbaaef1c6c58e56f28f01dc5b32ac1a798
Gerd Streubel <gst@pixel.de>
2019-12-03 19:05:27 +0100

    Day 01/12/19, Part 1+2

---
+++ src/main/java/de/streubel/aoc19/Day01.java
@@ -0,0 +1,43 @@
+package de.streubel.aoc19;
+
+import de.streubel.AdventOfCodeRunner;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Day01 extends AdventOfCodeRunner {
+
+    @Override
+    public void run(List<String> stringInput) {
+        int[] input = new int[stringInput.size()];
+        for (int i=0; i<input.length; i++) {
+            input[i] = Integer.parseInt(stringInput.get(i));
+        }
+
+        int fuelTotal = 0;
+        for (int moduleMass: input) {
+            fuelTotal += moduleMass / 3 - 2;
+        }
+        System.out.println("Result Part 1: "+fuelTotal);
+
+        fuelTotal = 0;
+        for (int moduleMass: input) {
+            int mass = moduleMass;
+            int fuel;
+
+            do {
+                fuel = mass / 3 - 2;
+                if (fuel > 0) {
+                    fuelTotal += fuel;
+                }
+                mass = fuel;
+            } while (fuel > 0);
+
+        }
+        System.out.println("Result Part 2: "+fuelTotal);
+
+
+    }
+
+}
