eec3deda0ae046cf65186cf86cc93e5391720288
Gerd Streubel <gst@pixel.de>
2019-12-06 20:55:14 +0100

    Day 06/12/19, Part 2

--- src/main/java/de/streubel/aoc19/Day06.java
+++ src/main/java/de/streubel/aoc19/Day06.java
@@ -1,5 +1,6 @@
 package de.streubel.aoc19;
 
+import com.google.common.collect.Lists;
 import de.streubel.AdventOfCodeRunner;
 
 import java.util.*;
@@ -21,6 +22,33 @@ public class Day06 extends AdventOfCodeRunner {
         System.out.println("Result Part 1 (241064): "+result);
 
 
+        final Orbiter you = Orbiter.find("YOU");
+        final Orbiter san = Orbiter.find("SAN");
+
+        final List<Orbiter> pathToYOU = new ArrayList<>();
+        final List<Orbiter> pathToSAN = new ArrayList<>();
+
+        com.goTo(you, pathToYOU);
+        com.goTo(san, pathToSAN);
+
+        final List<Orbiter> pathInCommon = new ArrayList<>(pathToYOU);
+        pathInCommon.retainAll(pathToSAN);
+
+        final List<Orbiter> pathFromForkToYOU = new ArrayList<>(pathToYOU);
+        pathFromForkToYOU.removeAll(pathInCommon);
+        pathFromForkToYOU.remove(you);
+
+        final List<Orbiter> pathFromForkToSAN = new ArrayList<>(pathToSAN);
+        pathFromForkToSAN.removeAll(pathInCommon);
+        pathFromForkToSAN.remove(san);
+
+        final List<Orbiter> pathFromYOUToSAN = new ArrayList<>();
+        pathFromYOUToSAN.addAll(Lists.reverse(pathFromForkToYOU));
+        // apparently the forkNode doesn't count to the path from YOU to SAN.
+        // pathFromYOUToSAN.add(Iterables.getLast(pathInCommon));
+        pathFromYOUToSAN.addAll(pathFromForkToSAN);
+
+        System.out.println("Result Part 2 (418): "+pathFromYOUToSAN.size());
     }
 
     public static class Orbiter {
@@ -58,7 +86,9 @@ public class Day06 extends AdventOfCodeRunner {
             getRoot().calculateDepth();
         }
 
-
+        static Orbiter find(final String name) {
+            return orbiterRepo.get(name);
+        }
 
         String name;
         Orbiter parent;
@@ -91,6 +121,46 @@ public class Day06 extends AdventOfCodeRunner {
         public void add(Orbiter o) {
             children.add(o);
             o.parent = this;
+        }
+
+        public boolean goTo(final Orbiter destination, final List<Orbiter> path) {
+            boolean found = false;
+            path.add(this);
+
+            if (this.equals(destination)) {
+                found = true;
+            } else {
+                for (Orbiter o : children) {
+                    found = o.goTo(destination, path);
+                    if (found) {
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    path.remove(this);
+                }
+            }
+
+            return found;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Orbiter orbiter = (Orbiter) o;
+            return name.equals(orbiter.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
         }
     }
 
